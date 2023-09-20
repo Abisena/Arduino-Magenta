89adebbe5658d894444c675a0384fe1e3de43121
Abisena <abisena1123@gmail.com>
2023-09-20 21:07:39 +0700

    Update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(createLamp);
 app.use(getAllLamps);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 db.on("error", (err) => {
   console.log(err);
--- controllers/arduinos.js
+++ controllers/arduinos.js
@@ -4,18 +4,17 @@ export const createLamp = async (req, res) => {
   try {
     const { item, status, time_at } = req.body;
     const newLamp = new Lamp({
-      item,
-      status,
-      time_at,
+      item: item,
+      status: status,
+      time_at: time_at,
     });
     await newLamp.save();
 
     res.status(201).json({ message: "Data berhasil dibuat", data: newLamp });
   } catch (error) {
     console.error(error);
-    res
-      .status(500)
-      .json({ message: "Terjadi kesalahan saat membuat data" });
+    res.status(500).json({ message: "Terjadi kesalahan saat membuat data" });
+    console.log(error);
   }
 };
 
