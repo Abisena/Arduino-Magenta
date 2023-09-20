3ca00f16e33fc5a67b0afcb1ae8c83d0594103a3
Abisena <abisena1123@gmail.com>
2023-09-20 21:11:42 +0700

    update

--- controllers/arduinos.js
+++ controllers/arduinos.js
@@ -2,14 +2,11 @@ import Lamp from "../models/arduino.js";
 
 export const createLamp = async (req, res) => {
   try {
-    const { 
-      item, 
-      status, 
-    } = req.body;
+    const { item, status } = req.body;
 
     const date = new Date();
     const time_at = date.setHours(date.getHours() + 7);
-    
+
     const newLamp = new Lamp({
       item: item,
       status: status,
@@ -18,42 +15,28 @@ export const createLamp = async (req, res) => {
 
     await newLamp.save();
 
-    res
-      .status(201)
-      .json({ 
-        message: "Data berhasil dibuat", 
-        data: newLamp, 
-      });
-
+    res.status(201).json({
+      message: "Data berhasil dibuat",
+      data: newLamp,
+    });
   } catch (error) {
     console.error(error);
-<<<<<<< HEAD
-res.status(500)
-      .json({ 
-        message: "Terjadi kesalahan saat membuat data" 
-      });
-  };
-=======
-
-    
->>>>>>> ce034b13a6a8d6edde4883c9854c4ba25f200e61
+    res.status(500).json({
+      message: "Terjadi kesalahan saat membuat data",
+    });
+  }
 };
 
 export const getAllLamps = async (req, res) => {
   try {
     const lamps = await Lamp.find();
 
-    res
-      .status(200)
-      .json(lamps);
-
+    res.status(200).json(lamps);
   } catch (error) {
-    res
-      .status(500)
-      .json({ 
-        error: "Gagal mendapatkan data" 
-      });
-  };
+    res.status(500).json({
+      error: "Gagal mendapatkan data",
+    });
+  }
 };
 
 export const deleteStatus = async (req, res) => {
@@ -61,11 +44,9 @@ export const deleteStatus = async (req, res) => {
     const { ids } = req.body;
 
     if (!ids || !Array.isArray(ids) || ids.length === 0) {
-      res
-        .status(400)
-        .json({ 
-          message: "Ids are required as an array" 
-        });
+      res.status(400).json({
+        message: "Ids are required as an array",
+      });
 
       return;
     }
@@ -73,28 +54,21 @@ export const deleteStatus = async (req, res) => {
     const delItems = await Lamp.find({ _id: { $in: ids } });
 
     if (delItems.length !== ids.length) {
-      res
-        .status(404)
-        .json({ 
-          message: "Some Ids not found" 
-        });
+      res.status(404).json({
+        message: "Some Ids not found",
+      });
 
       return;
     }
 
     await Lamp.deleteMany({ _id: { $in: ids } });
 
-    res
-      .status(200)
-      .json({ 
-        message: "Ids deleted successfully" 
-      });
-
+    res.status(200).json({
+      message: "Ids deleted successfully",
+    });
   } catch (error) {
-    res
-      .status(500)
-      .json({ 
-        message: "Internal Server Error"
-      });
-  };
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 };
