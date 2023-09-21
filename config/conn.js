2117dc91f0c901c00d10d1c309c597b82aab5c97
Abisena <abisena1123@gmail.com>
2023-09-21 13:31:58 +0700

    add connection

--- config/conn.js
+++ config/conn.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 mongoose.connect(
   "mongodb+srv://arduino:arduino12345@cluster0.cmqrijy.mongodb.net/arduino_lamp",
+  // "mongodb://localhost:27017/arduino_lamp",
   {
     useNewUrlparser: true,
     useUnifiedTopology: true,
